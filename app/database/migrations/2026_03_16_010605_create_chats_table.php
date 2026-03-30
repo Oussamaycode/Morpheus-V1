c1a53359ff47912d4c1ce6c2e61a3d7afba66593
Oussamaycode <oussamalamlouki047@gmail.com>
2026-03-30 10:23:59 +0000

    feat: enable chat request authorization, add validation rule for name, and include name column in chats table

--- app/database/migrations/2026_03_16_010605_create_chats_table.php
+++ app/database/migrations/2026_03_16_010605_create_chats_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->string('type')->default('private'); 
             $table->foreignId('owner_id')->constrained('users');
             $table->timestamps();
