7d9212f19559d0d22bcf1bb2656a58971cf467eb
Oussamaycode <oussamalamlouki047@gmail.com>
2026-03-30 11:16:04 +0000

    feat: add validation rules for user_id and chat_id in StoreMessageRequest

--- app/app/Http/Requests/StoreMessageRequest.php
+++ app/app/Http/Requests/StoreMessageRequest.php
@@ -22,6 +22,8 @@ class StoreMessageRequest extends FormRequest
     public function rules(): array
     {
         return [ 'content'=>['required','min:1','string'],
+                 'user_id'=>['integer','required'],
+                 'chat_id'=>['integer','required']
         ];
     }
 }
