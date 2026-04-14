462d2befcf99030a9160f6499143dd1ade03c9b0
Oussamaycode <oussamalamlouki047@gmail.com>
2026-04-14 21:51:30 +0000

    feat: add SessionController import to API routes

--- app/routes/api.php
+++ app/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ChatController;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\SessionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
