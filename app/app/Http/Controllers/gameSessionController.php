ee0cb1992f6d97ca64394a1bfbf74d4091f19142
Oussamaycode <oussamalamlouki047@gmail.com>
2026-04-29 10:39:37 +0000

    refactor: rename class and adjust import order in gameSession files

--- app/app/Http/Controllers/gameSessionController.php
+++ app/app/Http/Controllers/gameSessionController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
  
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use App\Models\gameSession;
 use App\Http\Requests\StoreGameSessionRequest;
+use App\Models\gameSession;
 
 class gameSessionController extends Controller
 {
