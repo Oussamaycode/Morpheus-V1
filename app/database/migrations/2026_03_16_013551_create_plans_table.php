e0c4986c6a169c5893da3ed47d873e8092987c37
Oussamaycode <oussamalamlouki047@gmail.com>
2026-04-29 11:26:01 +0000

    feat: add name column to plans table

--- app/database/migrations/2026_03_16_013551_create_plans_table.php
+++ app/database/migrations/2026_03_16_013551_create_plans_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('plans', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->integer('price');
             $table->string('duration')->default('1 month');
             $table->timestamps();
